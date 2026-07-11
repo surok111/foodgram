b30750993eed0131b04fc90f77439669ff5ae662
Ilya Sl <137293990+surok111@users.noreply.github.com>
2026-07-11 22:43:11 +0300

    Update index.js

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="#" className={styles.textLink}>https://github.com/surok111</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="#" className={styles.textLink}>surok111</a>
             </p>
           </div>
         </aside>
